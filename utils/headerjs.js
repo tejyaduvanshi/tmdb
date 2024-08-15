a7c3c9a5e7a4ff14406bcae77cfc0b4a6f49b4c3
tejyaduvanshi <tejyaduvanshi01@gmail.com>
2024-08-16 02:30:03 +0530

    tmdb

--- utils/headerjs.js
+++ utils/headerjs.js
@@ -1,8 +1,16 @@
-document.addEventListener("click", function (event) {
+// document.addEventListener("click", function (event) {
+//     const dropdown = document.querySelector(".dropdown");
+//     if (event.target !== dropdown) {
+//       const dropdownContent = document.querySelector(".dropdown-content");
+//       dropdownContent.style.display = "none";
+//     }
+//   });
+if (typeof window !== "undefined" && typeof document !== "undefined") {
+  document.addEventListener("click", function (event) {
     const dropdown = document.querySelector(".dropdown");
     if (event.target !== dropdown) {
       const dropdownContent = document.querySelector(".dropdown-content");
       dropdownContent.style.display = "none";
     }
   });
-  
+}
